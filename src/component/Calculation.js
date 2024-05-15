0d8c81f1680c70d3e046b7c4ca4dba2bc5e90707
Punam Gawade <141442107+punamgawade29@users.noreply.github.com>
2024-05-15 12:20:23 +0530

    Update Calculation.js

--- src/component/Calculation.js
+++ src/component/Calculation.js
@@ -5,7 +5,7 @@ const Calculation = () => {
   const [input, setInput] = useState('');
   const [result, setResult] = useState('');
 
-  const handleInput = (e) => {
+  const hInput= (e) => {
     setInput((prevInput) => prevInput + e.target.name);
   };
 
@@ -31,22 +31,22 @@ const Calculation = () => {
       </div>
       <div className="buttons">
         <button onClick={clear} className="button ac">AC</button>
-        <button name="/" onClick={handleInput} className="button operator">÷</button>
-        <button name="*" onClick={handleInput} className="button operator">×</button>
-        <button name="7" onClick={handleInput} className="button">7</button>
-        <button name="8" onClick={handleInput} className="button">8</button>
-        <button name="9" onClick={handleInput} className="button">9</button>
-        <button name="-" onClick={handleInput} className="button operator">-</button>
-        <button name="4" onClick={handleInput} className="button">4</button>
-        <button name="5" onClick={handleInput} className="button">5</button>
-        <button name="6" onClick={handleInput} className="button">6</button>
-        <button name="+" onClick={handleInput} className="button operator">+</button>
-        <button name="1" onClick={handleInput} className="button">1</button>
-        <button name="2" onClick={handleInput} className="button">2</button>
-        <button name="3" onClick={handleInput} className="button">3</button>
+        <button name="/" onClick={hInput} className="button operator">÷</button>
+        <button name="*" onClick={hInput} className="button operator">×</button>
+        <button name="7" onClick={hInput} className="button">7</button>
+        <button name="8" onClick={hInput} className="button">8</button>
+        <button name="9" onClick={hInput} className="button">9</button>
+        <button name="-" onClick={hInput} className="button operator">-</button>
+        <button name="4" onClick={hInput} className="button">4</button>
+        <button name="5" onClick={hInput} className="button">5</button>
+        <button name="6" onClick={hInput} className="button">6</button>
+        <button name="+" onClick={hInput} className="button operator">+</button>
+        <button name="1" onClick={hInput} className="button">1</button>
+        <button name="2" onClick={hInput} className="button">2</button>
+        <button name="3" onClick={hInput} className="button">3</button>
         <button onClick={calculate} className="button eqlval">=</button>
-        <button name="0" onClick={handleInput} className="button zero">0</button>
-        <button name="." onClick={handleInput} className="button">.</button>
+        <button name="0" onClick={hInput} className="button zero">0</button>
+        <button name="." onClick={hInput} className="button">.</button>
       </div>
     </div>
   );
